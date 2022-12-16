050f2d7fae4abbd7e1c703bb6cada69084125cea
ricle13 <soulderect@gmail.com>
2022-12-16 22:33:06 +0300

    Поправил фтльтр цензуры.

--- NewsPortal/news/templatetags/custom_filters.py
+++ NewsPortal/news/templatetags/custom_filters.py
@@ -4,11 +4,11 @@ register = template.Library()
 
 @register.filter()
 def censor(value):
-    censor_list = ('Cricket', 'ball', 'Soccer')
+    censor_list = ('cricket', 'ball', 'soccer')
     value = value.split()
     cens_value = ''
     for val in value:
-        if val not in censor_list:
+        if val.lower() not in censor_list:
             cens_value = cens_value + val + ' '
         else:
             cens_value = cens_value + val.replace(val[1::], '*'*(len(val)-1)) + ' '
